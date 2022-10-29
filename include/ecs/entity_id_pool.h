cdd283d89bf9e9b5428354658d890d42b1785bea
yan.mezhov <yan.mezhov@yandex.ru>
2022-10-29 20:10:07 +0200

    refactor entity id pool

--- include/ecs/entity_id_pool.h
+++ include/ecs/entity_id_pool.h
@@ -15,27 +15,9 @@ namespace ecs
     uint allocated = 0;
     uint used = 0;
     uint active = 0;
-    ~EntityPool()
-    {
-      uint last = used;
+    ~EntityPool();
 
-      for (auto batch : entities)
-      {
-        for (uint i = 0, n = std::min(entityBinSize, last); i < n; i++)
-        {
-          ECS_ASSERT(batch[i].state == EntityState::Destoyed);
-        }
-        last -= entityBinSize;
-
-        delete[] batch;
-      }
-    }
-
-    void allocate_more_entity()
-    {
-      allocated += entityBinSize;
-      entities.push_back(new EntityDescription[entityBinSize]);
-    }
+    void allocate_more_entity();
 
     void ECS_INLINE get_chunk_offset(uint idx, uint &chunk, uint &offset)
     {
@@ -43,37 +25,12 @@ namespace ecs
       offset = idx & entityBinMask;
     }
 
-    EntityDescription *allocate_entity()
-    {
-      if (used == allocated)
-        allocate_more_entity();
-      uint idx = used;
-      used++;
-      active++;
-      EntityDescription *entity = entities.back() + idx;
-      entity->state = EntityState::CreatedNotInited;
-      return entity;
-    }
+    EntityDescription *allocate_entity();
 
-    void deallocate_entity(EntityDescription *entity)
-    {
-      ECS_ASSERT(entity->valid_for_destroy());
-      entity->archetype = -1;
-      entity->index = -1;
-      entity->state = EntityState::Destoyed;
-      active--;
-    }
+    void deallocate_entity(EntityDescription *entity);
 
-    void deallocate_entity(EntityId eid)
-    {
-      ECS_ASSERT(eid.description);
-      deallocate_entity((EntityDescription *)eid.description);
-    }
+    void deallocate_entity(EntityId eid);
 
-    void add_entity_to_destroy_queue(EntityId eid)
-    {
-      ((EntityDescription *)eid.description)->state = EntityState::InDestroyQueue;
-      entitiesToDestroy.push(eid);
-    }
+    void add_entity_to_destroy_queue(EntityId eid);
   };
 }
--- sources/ecs/entity_id_pool.cpp
+++ sources/ecs/entity_id_pool.cpp
@@ -2,5 +2,58 @@
 
 namespace ecs
 {
+  EntityPool::~EntityPool()
+  {
+    uint last = used;
 
+    for (auto batch : entities)
+    {
+      for (uint i = 0, n = std::min(entityBinSize, last); i < n; i++)
+      {
+        ECS_ASSERT(batch[i].state == EntityState::Destoyed);
+      }
+      last -= entityBinSize;
+
+      delete[] batch;
+    }
+  }
+
+  void EntityPool::allocate_more_entity()
+  {
+    allocated += entityBinSize;
+    entities.push_back(new EntityDescription[entityBinSize]);
+  }
+
+  EntityDescription *EntityPool::allocate_entity()
+  {
+    if (used == allocated)
+      allocate_more_entity();
+    uint idx = used;
+    used++;
+    active++;
+    EntityDescription *entity = entities.back() + idx;
+    entity->state = EntityState::CreatedNotInited;
+    return entity;
+  }
+
+  void EntityPool::deallocate_entity(EntityDescription *entity)
+  {
+    ECS_ASSERT(entity->valid_for_destroy());
+    entity->archetype = -1;
+    entity->index = -1;
+    entity->state = EntityState::Destoyed;
+    active--;
+  }
+
+  void EntityPool::deallocate_entity(EntityId eid)
+  {
+    ECS_ASSERT(eid.description);
+    deallocate_entity((EntityDescription *)eid.description);
+  }
+
+  void EntityPool::add_entity_to_destroy_queue(EntityId eid)
+  {
+    ((EntityDescription *)eid.description)->state = EntityState::InDestroyQueue;
+    entitiesToDestroy.push(eid);
+  }
 }
