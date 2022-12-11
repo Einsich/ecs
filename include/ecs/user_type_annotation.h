21fe4ede71ea3a6795997e6aea5b2453457123b9
yan.mezhov <yan.mezhov@yandex.ru>
2022-12-11 22:43:13 +0100

    user type registr helper

--- include/ecs/user_type_annotation.h
+++ include/ecs/user_type_annotation.h
@@ -4,14 +4,14 @@
 
 namespace ecs
 {
-  template<typename Info>
+  template <typename Info>
   ecs::vector<Info> &get_all_user_info()
   {
     static ecs::vector<Info> user_info;
     return user_info;
   }
 
-  template<typename Info>
+  template <typename Info>
   void add_user_info(uint type_idx, Info &&info)
   {
     auto &infoList = get_all_user_info<Info>();
@@ -19,7 +19,7 @@ namespace ecs
     infoList[TypeIndex<T>::value] = info;
   }
 
-  template<typename T, typename Info>
+  template <typename T, typename Info>
   void add_user_info(Info &&info)
   {
     auto &infoList = get_all_user_info<Info>();
@@ -27,15 +27,28 @@ namespace ecs
     infoList[type_idx] = info;
   }
 
-  template<typename Info>
+  template <typename Info>
   Info &get_user_info(int type_idx)
   {
     return get_all_user_info<Info>()[type_idx];
   }
 
-  template<typename T, typename Info>
+  template <typename T, typename Info>
   Info &get_user_info()
   {
     return get_all_user_info<T>()[TypeIndex<T>::value];
   }
+
+  template <typename T, typename Info>
+  struct UserInfoRegistrationHelper
+  {
+    UserInfoRegistrationHelper(Info &&info)
+    {
+      add_user_info<T, Info>(std::move(info));
+    }
+  };
+
+#define ECS_USER_TYPE_REGISTRATION(TYPE, INFO) \
+  static ecs::UserInfoRegistrationHelper<TYPE> \
+      __CONCAT__(user_type_registrator, __LINE__)(INFO);
 }
