58236491c634b51b08832afcf6404adaa7e588e6
yan.mezhov <yan.mezhov@yandex.ru>
2023-01-06 01:02:17 +0100

    type updating

--- include/ecs/type_description_storage.h
+++ include/ecs/type_description_storage.h
@@ -20,6 +20,21 @@ namespace ecs
       ECS_ASSERT((int)allRegisteredTypes.size() == type_index);
       allRegisteredTypes.push_back(std::move(annotation));
     }
+    
+    void update_type(int type_index, Description &&annotation)
+    {
+      ECS_ASSERT_RETURN((size_t)type_index < allRegisteredTypes.size(), );
+      for (auto it = typeNameToIndex.begin(), to = typeNameToIndex.end(); it != to; ++it)
+      {
+        if (it->second == type_index && it->first != annotation.name)
+        {
+          typeNameToIndex.erase(it);
+          typeNameToIndex.emplace(annotation.name, type_index);
+          break;
+        }
+      }
+      new (allRegisteredTypes.data() + type_index) Description(std::move(annotation));
+    }
 
     int type_name_to_index(const char *name) const
     {
@@ -41,6 +56,7 @@ namespace ecs
 }
 
 #define USE_DESCRIPTION_STORAGE(T,                                    \
+                                update_type_func,                     \
                                 register_type_func,                   \
                                 type_name_to_index_func,              \
                                 get_all_registered_types_func,        \
@@ -55,6 +71,10 @@ namespace ecs
     void register_type_func(int type_index, T annotation)             \
     {                                                                 \
       get_storage().register_type(type_index, std::move(annotation)); \
+    }                                                                 \
+    void update_type_func(int type_index, T annotation)               \
+    {                                                                 \
+      get_storage().update_type(type_index, std::move(annotation));   \
     }                                                                 \
     int type_name_to_index_func(const char *name)                     \
     {                                                                 \
